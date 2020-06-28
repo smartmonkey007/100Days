c8c5cd409aae4a2c9f2d5feaa6ec53868367f638
meugnim <marc.meugniot@igt.com>
2020-06-28 16:29:17 -0700

    added AppBar

--- projects/rock-n-roll/src/App.vue
+++ projects/rock-n-roll/src/App.vue
@@ -1,32 +1,36 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <AppBar
+      brandName="Rock-n-Roll"
+      v-bind:menuItems="[{ caption: 'Home', link: '/' }, { caption: 'About', link: '/about' }]"
+    />
+    <router-view />
   </div>
 </template>
 
+<script lang="ts">
+import Vue from 'vue';
+import AppBar from '@/components/AppShell/AppBar.vue';
+
+export default Vue.extend({
+  components: {
+    AppBar,
+  }
+});
+</script>
+
 <style lang="scss">
+html,
+body {
+  padding: 0;
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
---
+++ projects/rock-n-roll/src/components/AppShell/AppBar.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="app-bar">
+    <span class="app-name">{{brandName}}</span>
+    <div class="menu-links">
+      <div v-for="menuItem in menuItems" :key="menuItem">
+        <router-link class="menu-link" v-bind:to="menuItem.link">{{menuItem.caption}}</router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+
+import { Component, Vue, Prop, } from 'vue-property-decorator';
+
+export class MenuItem {
+  caption?: string;
+  link?: string;
+}
+
+@Component
+export default class AppBar extends Vue {
+  @Prop() brandName: string;
+  @Prop() menuItems?: MenuItem[];
+}
+
+</script>
+
+<style lang="scss" scoped>
+.app-name {
+  font-size: xx-large;
+  align-self: center;
+}
+
+.menu-links {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+
+.menu-link {
+  background-color: pink;
+  padding: 10px;
+  margin: 10px 30px;
+  text-decoration: none;
+  font-size: larger;
+  font-weight: bolder;
+
+  &.router-link-exact-active {
+    color: teal;
+  }
+}
+
+.app-bar {
+  box-sizing: border-box;
+  padding-left: 15px;
+  width: 100%;
+  background-color: rgb(243, 3, 135);
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  height: 70px;
+}
+</style>
